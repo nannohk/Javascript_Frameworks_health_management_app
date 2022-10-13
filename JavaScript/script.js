40081e0b64cb1710a45de11ed8aaa9a5caffee50
nannohk <nannohkw@unb.ca>
2022-10-13 00:26:04 -0300

    Vue counter application complete

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,14 @@
-let clickbutton = document.getElementById("btn");
-let counter = 0;
+// let clickbutton = document.getElementById("btn");
+// let counter = 0;
 
-clickbutton.addEventListener("click", function() {
-    counter++;
-    console.log(counter);
-    document.getElementById('clickedNum').innerHTML = "You clicked the button " + counter + " times";});
+// clickbutton.addEventListener("click", function() {
+//     counter++;
+//     console.log(counter);
+//     document.getElementById('clickedNum').innerHTML = "You clicked the button " + counter + " times";});
+
+    // var app = new Vue({
+    //     el: '#clickedNum',
+    //     data: {
+    //         counter: 0
+    //     }
+    // })
