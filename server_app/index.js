fcb3f1d98b9230cf8b1996810cde1a66f63c55ad
nannohk <nannohkw@unb.ca>
2022-11-07 09:34:37 -0400

    Inserting into the database..except the license :(

--- server_app/index.js
+++ server_app/index.js
@@ -9,19 +9,38 @@ app.listen(1500, () => console.log('Server running on port 1500'));
 
 /***************************************************************************************************** */
 //Database information
-// db.run('CREATE TABLE users (email Varchar2(100) PRIMARY KEY, password Varchar2(100))');
+//db.run('drop table if exists users');
+
+// db.run('CREATE TABLE users (email Varchar2(100) PRIMARY KEY, password Varchar2(100),fullname varchar2(200),
+//address varchar2(200),gender varchar2(10),license boolean)');
+
 // db.close((err) => {
 //     if (err) return console.error(err.message);
 // });
 
 let authStatus = false;
 const sqlInt = `INSERT INTO users (email, password) VALUES(?,?)`;
+const sqlUpdate = `UPDATE users SET fullname = ?,address=?, gender=?, license=? WHERE email = ?`;
 const sqlSelect = `SELECT email, password FROM users WHERE email = ? and password = ?`;
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./datastore/serverDB.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) return console.error(err.message);
     console.log('Connected to the project database.');
 });
+
+function updateData(email, fullname, address, gender, license) {
+    return new Promise((resolve, reject) => {
+        db.run(sqlUpdate, [fullname, address, gender, license, email], function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({
+                    message: 'profile updated'
+                });
+            }
+        });
+    });
+}
 
 function insertData(email, password) {
     db.run(sqlInt, [email, password],
@@ -65,6 +84,7 @@ app.post('/', (request, response) => {
     }
     else if (request.body.purpose == "login") {
         console.log("login request");
+        // insertData('test@gmail.com', 'test');
         selectData(request.body.email, request.body.password).then((sol) => {
             console.log("login success");
             response.json({
@@ -77,11 +97,19 @@ app.post('/', (request, response) => {
             });
         });
     }
-    else if(request.body.purpose == "profileUpdate"){
+    else if (request.body.purpose == "profileUpdate") {
         console.log("profile update request");
-        response.json({
-            status: 'success',
+        updateData(request.body.email, request.body.fullName, request.body.address, request.body.gender, request.body.license).then((sol) => {
+            console.log("profile update success");
+            response.json({
+                status: 'success',
+            });
+        }).catch((err) => {
+            console.log("profile update failed");
+            response.json({
+                status: 'failed',
+            });
         });
     }
 
-    });
+});
