ddc5c6acc94081e76c67526ce7b8fd1a5c4cdf10
nannohk <nannohkw@unb.ca>
2022-12-01 23:31:26 -0400

    Forgot to add emergency contact

--- vue-login_app/src/pages/patients-page.vue
+++ vue-login_app/src/pages/patients-page.vue
@@ -1,116 +1,121 @@
 <template>
-    <h1>Patient Form</h1>
-    <div class="card">
-        <MDBAccordion v-model="activeItem" flush>
-            <MDBAccordionItem headerTitle="Patient Information" collapseId="patient">
-                <form>
-                    <div class="row">
-                        <div class="col-5">
-                            <label class="col-form-label">Full Name</label>
-                            <input type="text" class="form-control" placeholder="Full Name">
-                        </div>
-                        <div class="col-2">
-                            <label class="col-form-label">Gender</label>
-                            <select class="form-control" v-model="gender">
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="other">Other</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-3">
-                            <label class="col-form-label">Date of Birth</label>
-                            <input type="text" class="form-control" placeholder="Date of Birth (YYYY-MM-DD)">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-7">
-                            <label class="col-form-label">Address</label>
-                            <input type="text" class="form-control" placeholder="211B Baker Street London, England">
-                        </div>
-                        <div class="form-group col-3">
-                            <label class="col-form-label">Phone Number</label>
-                            <input type="text" class="form-control" placeholder="506 555 5555">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-2">
-                            <label class="col-form-label">Employer</label>
-                            <input type="text" class="form-control" placeholder="UNB">
-                        </div>
-                        <div class="form-group col-5">
-                            <label class="col-form-label">Employer Address</label>
-                            <input type="text" class="form-control" placeholder="100 Tucker Park Road">
-                        </div>
-                        <div class="form-group col-3">
-                            <label class="col-form-label">Position</label>
-                            <input type="text" class="form-control" placeholder="Professor">
-                        </div>
-                    </div>
-                </form>
-            </MDBAccordionItem>
+    <div class="container py-5 justify-content-center align-items-center">
+        <div class="text-center">
+            <h1>Patient Information</h1>
+            <hr />
+        </div>
 
-            <MDBAccordionItem headerTitle="Emergency Contact Information" collapseId="emergency">
-                <form>
-                    <div class="row">
-                        <div class="col-8">
-                            <label class="col-form-label">Insurance Company</label>
-                            <input type="text" class="form-control" placeholder="Insurance Company Name">
+        <div class="card">
+            <MDBAccordion v-model="activeItem" stayOpen>
+                <MDBAccordionItem headerTitle="Patient Information" collapseId="patient">
+                    <form class="row container h-100  justify-content-center align-items-center">
+                        <div class="row">
+                            <div class="col">
+                                <label class="col-form-label">Full Name</label>
+                                <input type="text" class="form-control" placeholder="Full Name">
+                            </div>
+                            <div class="col">
+                                <label class="col-form-label">Gender</label>
+                                <select class="form-control" v-model="gender">
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Date of Birth</label>
+                                <input type="text" class="form-control" placeholder="Date of Birth (YYYY-MM-DD)">
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-2">
-                            <label class="col-form-label">Policy Number</label>
-                            <input type="text" class="form-control" placeholder="86464 687815 89846">
+                        <div class="row">
+                            <div class="form-group col">
+                                <label class="col-form-label">Address</label>
+                                <input type="text" class="form-control" placeholder="211B Baker Street London, England">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Phone Number</label>
+                                <input type="text" class="form-control" placeholder="506 555 5555">
+                            </div>
                         </div>
-                        <div class="col-2">
-                            <label class="col-form-label">Group Number</label>
-                            <input type="text" class="form-control" placeholder="1738 5494">
+                        <div class="row">
+                            <div class="form-group col">
+                                <label class="col-form-label">Employer</label>
+                                <input type="text" class="form-control" placeholder="UNB">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Employer Address</label>
+                                <input type="text" class="form-control" placeholder="100 Tucker Park Road">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Position</label>
+                                <input type="text" class="form-control" placeholder="Professor">
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-7">
-                            <label class="col-form-label">Insured Party</label>
-                            <input type="text" class="form-control" placeholder="Insured Party">
-                        </div>
-                        <div class="form-group col-3">
-                            <label class="col-form-label">Relationship</label>
-                            <input type="text" class="form-control" placeholder="Brother/Sister/...">
-                        </div>
-                    </div>
-                </form>
-            </MDBAccordionItem>
-            <MDBAccordionItem headerTitle="Insurance" collapseId="insurance">
-                <form>
-                    <div class="row">
-                        <div class="col">
-                            <label class="col-form-label">Insurance Company</label>
-                            <input type="text" class="form-control" placeholder="Insurance Company Name">
-                        </div>
-                        <div class="form-group col">
-                            <label class="col-form-label">Insured Party</label>
-                            <input type="text" class="form-control" placeholder="Insured Party">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <label class="col-form-label">Policy Number</label>
-                            <input type="text" class="form-control" placeholder="86464 687815 89846">
-                        </div>
-                        <div class="col">
-                            <label class="col-form-label">Group Number</label>
-                            <input type="text" class="form-control" placeholder="1738 5494">
-                        </div>
-                    </div>
-                    <div class="row">
+                    </form>
+                </MDBAccordionItem>
 
-                        <div class="form-group col">
-                            <label class="col-form-label">Relationship</label>
-                            <input type="text" class="form-control" placeholder="Brother/Sister/...">
+                <MDBAccordionItem headerTitle="Emergency Contact Information" collapseId="emergency">
+                    <form class="row container h-100  justify-content-center align-items-center">
+                        <div class="row">
+                            <div class="col-8">
+                                <label class="col-form-label">Insurance Company</label>
+                                <input type="text" class="form-control" placeholder="Insurance Company Name">
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </MDBAccordionItem>
-        </MDBAccordion>
+                        <div class="row">
+                            <div class="col-2">
+                                <label class="col-form-label">Policy Number</label>
+                                <input type="text" class="form-control" placeholder="86464 687815 89846">
+                            </div>
+                            <div class="col-2">
+                                <label class="col-form-label">Group Number</label>
+                                <input type="text" class="form-control" placeholder="1738 5494">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-7">
+                                <label class="col-form-label">Insured Party</label>
+                                <input type="text" class="form-control" placeholder="Insured Party">
+                            </div>
+                            <div class="form-group col-3">
+                                <label class="col-form-label">Relationship</label>
+                                <input type="text" class="form-control" placeholder="Brother/Sister/...">
+                            </div>
+                        </div>
+                    </form>
+                </MDBAccordionItem>
+                <MDBAccordionItem headerTitle="Insurance" collapseId="insurance">
+                    <form class="row container h-100  justify-content-center align-items-center">
+                        <div class="row">
+                            <div class="col">
+                                <label class="col-form-label">Insurance Company</label>
+                                <input type="text" class="form-control" placeholder="Insurance Company Name">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Insured Party</label>
+                                <input type="text" class="form-control" placeholder="Insured Party">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <label class="col-form-label">Policy Number</label>
+                                <input type="text" class="form-control" placeholder="86464 687815 89846">
+                            </div>
+                            <div class="col">
+                                <label class="col-form-label">Group Number</label>
+                                <input type="text" class="form-control" placeholder="1738 5494">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Relationship</label>
+                                <input type="text" class="form-control" placeholder="Brother/Sister/...">
+                            </div>
+                        </div>
+
+
+                    </form>
+                </MDBAccordionItem>
+            </MDBAccordion>
+        </div>
     </div>
 </template>
 
@@ -129,7 +134,7 @@ export default {
     data() {
         return {
             users: [],
-            activeItem: ref('insurance')
+            activeItem: ref('patient')
         }
     },
     methods: {
