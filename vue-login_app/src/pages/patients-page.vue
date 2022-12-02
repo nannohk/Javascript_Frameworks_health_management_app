f0fece2f6bf87327c77b2974c3981644e963da0f
nannohk <nannohkw@unb.ca>
2022-12-01 23:39:58 -0400

    Patient form completed.Adding extra DB columns

--- vue-login_app/src/pages/patients-page.vue
+++ vue-login_app/src/pages/patients-page.vue
@@ -57,30 +57,25 @@
                 <MDBAccordionItem headerTitle="Emergency Contact Information" collapseId="emergency">
                     <form class="row container h-100  justify-content-center align-items-center">
                         <div class="row">
-                            <div class="col-8">
-                                <label class="col-form-label">Insurance Company</label>
-                                <input type="text" class="form-control" placeholder="Insurance Company Name">
+                            <div class="col">
+                                <label class="col-form-label">Full Name</label>
+                                <input type="text" class="form-control" placeholder="Full Name">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-2">
-                                <label class="col-form-label">Policy Number</label>
-                                <input type="text" class="form-control" placeholder="86464 687815 89846">
-                            </div>
-                            <div class="col-2">
-                                <label class="col-form-label">Group Number</label>
-                                <input type="text" class="form-control" placeholder="1738 5494">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="form-group col-7">
-                                <label class="col-form-label">Insured Party</label>
-                                <input type="text" class="form-control" placeholder="Insured Party">
-                            </div>
-                            <div class="form-group col-3">
+                            <div class="col">
                                 <label class="col-form-label">Relationship</label>
                                 <input type="text" class="form-control" placeholder="Brother/Sister/...">
                             </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col">
+                                <label class="col-form-label">Address</label>
+                                <input type="text" class="form-control" placeholder="211B Baker Street London, England">
+                            </div>
+                            <div class="form-group col">
+                                <label class="col-form-label">Phone Number</label>
+                                <input type="text" class="form-control" placeholder="506 555 5555">
+                            </div>
+
                         </div>
                     </form>
                 </MDBAccordionItem>
